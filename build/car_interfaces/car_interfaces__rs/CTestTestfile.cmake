42f8f088848b3cbe92a11ec8dab96237bbb92431
rhobtor <luismidp7@gmail.com>
2026-04-10 01:58:32 +0200

    upate gui , fsm , and docs

--- build/car_interfaces/car_interfaces__rs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rhobtor/PHD/ARGOJ8_IA/ARGOSJ8_IA_/build/car_interfaces/car_interfaces__rs
-# Build directory: /home/rhobtor/PHD/ARGOJ8_IA/ARGOSJ8_IA_/build/car_interfaces/car_interfaces__rs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/fixposition_driver_ros2/fixposition_driver_ros2__rs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rhobtor/PHD/ARGOJ8_IA/ARGOSJ8_IA_/build/fixposition_driver_ros2/fixposition_driver_ros2__rs
-# Build directory: /home/rhobtor/PHD/ARGOJ8_IA/ARGOSJ8_IA_/build/fixposition_driver_ros2/fixposition_driver_ros2__rs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
